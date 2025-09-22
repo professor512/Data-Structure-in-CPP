6f7f0da4128b2440ea877fb08976a4dae33ddab0
Karan Salunkhe <karansalunkhe180@gmail.com>
2025-09-22 14:56:28 +0530

    Create STL_multimap.cpp

---
+++ STL_multimap.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // Can store Duplicate keys but in sorted order
+    // everything same as map, it can store multiple keys
+    // only mpp[keys] cannot be used here
+    
+
+}
