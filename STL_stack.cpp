d49db2376811281b7edeb1da2bec170b797db475
Karan Salunkhe <karansalunkhe180@gmail.com>
2025-09-22 12:17:04 +0530

    Create STL_stack.cpp

---
+++ STL_stack.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // LIFO
+
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(4);
+    st.emplace(6);
+
+    cout<<st.top()<<endl;
+    
+    st.pop();
+    
+    cout<<st.top()<<endl;
+
+    cout<<st.size()<<endl;
+
+    cout<< st.empty() << endl; 
+
+    stack<int> st1, st2;
+    st1.swap(st2);
+
+}
