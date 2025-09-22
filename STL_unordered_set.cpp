fe165e271995627b44ca55495ebc31b4656a00ab
Karan Salunkhe <karansalunkhe180@gmail.com>
2025-09-22 13:01:44 +0530

    Create STL_unordered_set.cpp

---
+++ STL_unordered_set.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   unordered_set<int> ust; 
+
+    // lower_bound and upper-bound function does not work
+    // rest all functions are same as above, it does not store any particular order, 
+    // it has a better complexity than set in most cases, except some when collision happens
+
+}
