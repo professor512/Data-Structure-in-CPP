bbefa076fa066f34012863466dc0d548118687d6
Karan Salunkhe <karansalunkhe180@gmail.com>
2025-10-21 15:29:52 +0530

    Implement rotateArray function for k-place rotation
    Added a new function to rotate an array by k places.

---
+++ Array/Easy/rotate_array_by_k_places.cpp
@@ -0,0 +1,22 @@
+// https://www.naukri.com/code360/problems/rotate-array_1230543?leftPanelTabValue=PROBLEM
+vector<int> rotateArray(vector<int>arr, int k) {
+    
+    int n = arr.size();
+    k = k % n;
+
+    int temp[k];
+
+    for(int i = 0; i < k; i++){
+        temp[i] = arr[i];
+    }
+
+    for(int i = k; i < n; i++){
+        arr[i-k] = arr[i];
+    }
+
+    for(int i = n - k; i < n; i++){
+        arr[i] = temp[i-(n-k)];
+    }
+
+    return arr;
+}
