84e1de70345c83e496b462c4b75697bea0865e44
Karan Salunkhe <karansalunkhe180@gmail.com>
2025-10-28 14:04:56 +0530

    Comment out URL in missing_number.cpp
    Commented out the URL reference for the missing number problem.

---
+++ Array/Easy/missing_number.cpp
@@ -0,0 +1,12 @@
+// https://www.naukri.com/code360/problems/missing-number_6680467
+
+int missingNumber(vector<int>&a, int N) {
+    int xor1 = 0, xor2 = 0;
+    int n = N - 1;
+    for(int i = 0; i < n; i++){
+        xor2 = xor2 ^ a[i];
+        xor1 = xor1 ^ (i+1);
+    }
+    xor1 = xor1 ^ N;
+    return xor1 ^ xor2;
+}
